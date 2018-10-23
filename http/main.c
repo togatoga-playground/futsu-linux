dc9d0f23043a5c4c22ccf45089cf3b3d7743f621
togatoga <togasakitogatoga@gmail.com>
2018-10-23 18:13:45 +0900

    [WIP] Impl http

--- http/main.c
+++ http/main.c
@@ -1,16 +1,27 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include <signal.h>
+#include <stdarg.h>
 #include <memory.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 
+typedef void (*sighandler_t) (int);
 static void log_exit(char *fmt, ...);
 static void *xmalloc(size_t sz);
 static void install_signal_handlers(void);
 static void trap_signal(int sig, sighandler_t handler);
 static void signal_exit(int sig);
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <docroot>\n", argv[0]);
+        exit(1);
+    }
+    install_signal_handlers();
+    exit(0);
 }
 
 static void log_exit(char *fmt, ...)
@@ -37,6 +48,17 @@ static void *xmalloc(size_t sz)
 static void install_signal_handlers(void)
 {
     trap_signal(SIGPIPE, signal_exit);
+}
+
+static void trap_signal(int sig, sighandler_t handler)
+{
+    struct sigaction act;
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    if (sigaction(sig, &act, NULL) < 0)
+    {
+        log_exit("sigaction() failed: %s", strerror(errno));
+    }
 }
 
 static void signal_exit(int sig)
